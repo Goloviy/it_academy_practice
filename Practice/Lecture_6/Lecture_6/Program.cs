7e33e6ca1eb2510986e16f8ac7dc0f838bf4e761
Ivan <goloviy@tut.by>
2019-12-19 21:02:02 +0300

    Сделал не доконца.

---
+++ Practice/Lecture_6/Lecture_6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lecture_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { char letter;
+            Console.WriteLine("Введите несколько слов:");
+            string str = Console.ReadLine();
+            for (int i = 0; i > str.Length; i++)
+            {
+                int quantity = 0;
+                if (str.Contains("a"))
+                {
+                    quantity ++;
+                }
+                i++;
+                
+            }
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
