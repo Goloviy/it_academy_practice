45f807e62b1a52e31978d470d06678116dc2b116
Ivan <goloviy@tut.by>
2019-12-24 19:18:15 +0300

    Task 2

--- Practice/lecture_7/lecture7/Program.cs
+++ Practice/lecture_7/lecture7/Program.cs
@@ -10,5 +10,15 @@ namespace lecture7
             // для челочисленных значений будет значение 0.
             int[] array = new int[10];
         }
+
+        static void Task2 ()
+        {
+            object[] array = new object[3];
+
+            array[0] = 32;
+            array[1] = 'A';
+            array[2] = "Hello";
+        }
+
     }
 }
